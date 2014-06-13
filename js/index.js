5197a40a67043ade614bda4e400634390cb2926c
Diego Ponce <diegoponce@outlook.com>
2014-06-13 00:32:41 -0500

    Change the grid on the intro area using semantic-ui properly

---
+++ js/index.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('.scrollup').fadeIn();
+        } else {
+            $('.scrollup').fadeOut();
+        }
+    });
+
+    $('.scrollup').click(function () {
+        $("html, body").animate({
+            scrollTop: 0
+        }, 800);
+        return false;
+    });
+
+});
+
+
